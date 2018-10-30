138fd273ef38b21e1dc1a15f0ebafa0a3ff9af73
Justin Lyons <jlyons@hueyscomputers.com>
2018-10-30 15:08:12 -0400

    Add image uploading

--- craigslist.py
+++ craigslist.py
@@ -38,7 +38,7 @@ class craigslistBot:
         # self.client = webdriver.Firefox()
         with open('config.yaml') as fp:
             data = yaml.load(fp)
-        self.waitTime = 0
+        self.waitTime = 1
         self.client = webdriver.Chrome()
         self.isLoggedIn = False
         self.loginEmail = data['clistLoginEmail']
@@ -160,67 +160,39 @@ class craigslistBot:
         menu = self.client.find_element_by_xpath("//label[contains(@class, 'year')]")
         self.dropdown(menu, self.modelYear)
         self.client.find_element_by_css_selector("#PostingBody").send_keys(self.postContent)
+        self.debug("Checking 'Okay to contact by phone'")
+        self.client.find_element_by_xpath("//input[contains(@name, 'contact_phone_ok')]").click()
+        time.sleep(self.waitTime)
+        self.debug("Checking 'Okay to contact by text'")
+        self.client.find_element_by_xpath("//input[contains(@name, 'contact_text_ok')]").click()
+        time.sleep(self.waitTime)
+        self.debug("Filling in contact phone number")
+        self.client.find_element_by_xpath("//input[@name='contact_phone']").send_keys(self.contactNumber)
+        time.sleep(self.waitTime)
+        self.debug("Filling in contact name")
+        self.client.find_element_by_xpath("//input[contains(@name, 'contact_name')]").send_keys(self.contactName)
+        time.sleep(self.waitTime)
+
+        self.debug("Clicking continue")
         self.client.find_element_by_css_selector('button[value="continue"]').click()
+        self.debug("Clicking continue after map")
+        self.client.find_element_by_xpath("//button[contains(@class, 'continue')]").click()
+        self.debug("Upload Images from directory")
+
+        cwd = os.getcwd()
+        for i in os.listdir('photos'):
+            path = cwd + "/photos/" + i
+            self.client.find_element_by_xpath("//input[@type='file']").send_keys(path)
 
         time.sleep(20)
-        # self.debug("Checking 'Okay to contact by phone'")
-        # self.client.find_element_by_css_selector("#contact_phone_ok").click()
-        # time.sleep(self.waitTime)
-        # self.debug("Checking 'Okay to contact by text'")
-        # self.client.find_element_by_css_selector("#contact_text_ok").click()
-        # time.sleep(self.waitTime)
-        # self.debug("Filling in contact phone number")
-        # self.client.find_element_by_css_selector("#contact_phone").send_keys(self.contactNumber)
-        # time.sleep(self.waitTime)
-        # self.debug("Filling in contact name")
-        # self.client.find_element_by_css_selector("#contact_name").send_keys(self.contactName)
-        # time.sleep(self.waitTime)
-        # self.debug("Filling in post title")
-    #     self.client.find_element_by_css_selector("#PostingTitle").send_keys(spintax.parse(self.postTitle))
-    #     time.sleep(self.waitTime)
-    #     self.debug("Filling in zip code")
-    #     self.client.find_element_by_css_selector("#postal_code").send_keys(self.postCode)
-    #     time.sleep(self.waitTime)
-    #
-    #     self.debug("Getting post content")
-    #     f = open(self.postContent, "rb")
-    #     content = f.read()
-    #     f.close()
-    #
-    #     self.debug("Spinning content")
-    #     spinContent = spintax.parse(content)
-    #
-    #     self.debug("Filling in post content")
-    #     self.client.find_element_by_css_selector("#PostingBody").send_keys(spinContent)
-    #     time.sleep(self.waitTime)
-    #     self.debug("Checking 'Okay to contact for other offers'")
-    #     self.client.find_element_by_css_selector("#oc").click()
-    #     time.sleep(self.waitTime)
-    #     self.debug("Unchecking 'Want a map' if checked")
-    #     try:
-    #         self.client.find_element_by_css_selector("#wantamap:checked")
-    #     except NoSuchElementException:
-    #         self.debug("Not checked")
-    #     finally:
-    #         self.client.find_element_by_css_selector("#wantamap:checked").click()
-    #     time.sleep(self.waitTime)
-    #     self.debug("Clicking continue")
-    #     self.client.find_element_by_css_selector('button[value="Continue"]').click()
-    #     time.sleep(self.waitTime)
-    #     if "editimage" in self.client.current_url:
-    #         self.debug("Clicking continue")
-    #         self.client.find_element_by_css_selector('button.done').click()
-    #     time.sleep(self.waitTime)
-    #     self.debug("Clicking publish")
-    #     self.client.find_element_by_css_selector('.draft_warning button[value="Continue"]').click()
-    #     time.sleep(10)
+
 
 
 def main():
     startExecTime = time.time()
 
     clBot = craigslistBot()
-    # clBot.login()
+    clBot.login()
     clBot.createPost()
     endExecTime = time.time()
     clBot.debug("Execution time: %s seconds" % int(endExecTime - startExecTime))
